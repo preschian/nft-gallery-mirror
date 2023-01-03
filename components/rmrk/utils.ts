dce23ac23a83d36bfa3df75a8a6db3df57dc8c72
Preschian Febryantara <preschian27@gmail.com>
2023-01-03 16:20:01 +0700

    refactor: remove unused

--- components/rmrk/utils.ts
+++ components/rmrk/utils.ts
@@ -30,7 +30,6 @@ export const ipfsProviders: Record<IPFSProviders, string> = {
   cloudflare: 'https://cloudflare-ipfs.com/',
   ipfs: DEFAULT_IPFS_PROVIDER,
   dweb: 'https://dweb.link/',
-  // kodadot: 'https://cdn.preschian.xyz/',
   kodadot: 'https://cdn.preschian-cdn.workers.dev/',
 }
 
