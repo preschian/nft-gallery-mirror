b90c8fe1dbe5a581f53107a02a91161466823db2
Preschian F <preschian27@gmail.com>
2023-02-11 14:39:10 +0700

    revert: add back

--- components/landing/SearchLanding.vue
+++ components/landing/SearchLanding.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="landing-search is-flex is-align-items-center">
+    <img src="/landing-blurred-header-left.svg" class="landing-search-left" />
+    <img :src="landingImage[0]" class="landing-shapes" />
     <div
       class="is-flex is-flex-direction-column is-align-items-center search-info">
       <h1
@@ -31,6 +33,8 @@
         </a>
       </div>
     </div>
+    <img src="/landing-blurred-header-right.svg" class="landing-search-right" />
+    <img :src="landingImage[1]" class="landing-shapes" />
   </section>
 </template>
 
