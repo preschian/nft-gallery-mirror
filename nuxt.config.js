3d30fc290de2d8de167227d094267d9b792cac7c
Preschian F <preschian27@gmail.com>
2023-04-05 20:37:24 +0700

    ci: disable static

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  // target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
