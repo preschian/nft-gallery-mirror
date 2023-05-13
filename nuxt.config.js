c5a20f7613ebbcf1b46dc27eff7dc5ee200ab72d
Preschian Febryantara <preschian@users.noreply.github.com>
2023-05-13 13:34:14 +0700

    refactor: checkpopint

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,7 +129,7 @@ export default defineNuxtConfig({
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['styles/index.scss'],
+  css: ['@/styles/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -207,9 +207,6 @@ export default defineNuxtConfig({
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/apollo',
-    '@nuxtjs/i18n',
-    '@nuxtjs/sentry',
     '@kevinmarrec/nuxt-pwa',
     '@nuxtjs/color-mode',
     '@vueuse/nuxt',
