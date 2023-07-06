6cd0e5fb327b1e2a5e16e957a1532d8a1c6c26a2
Preschian Febryantara <preschian@users.noreply.github.com>
2023-07-06 15:30:52 +0700

    feat: gradually migrate nuxt 3

---
+++ apps/marketplace/pages/[prefix]/u/[address].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>prefix: {{ route.params.prefix }}</p>
+    <p>address: {{ route.params.address }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+console.log(route.params)
+</script>
